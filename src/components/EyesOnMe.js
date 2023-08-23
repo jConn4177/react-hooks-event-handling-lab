98617265291e2d43b7484b46f787cba4d7545237
Jason <jconn99801@gmail.com>
2023-08-22 18:42:11 -0800

    initial commit to to prevent errors

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,7 @@
-// Code EyesOnMe Component Here
+import React from "react";
+
+function EyesOnMe() {
+  return <div></div>;
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,7 @@
-// Code Keypad Component Here
+import React from "react";
 
-function Keypad (){
-    return (
-        <div></div>
-    )
+function Keypad() {
+  return <div></div>;
 }
 
 export default Keypad;
